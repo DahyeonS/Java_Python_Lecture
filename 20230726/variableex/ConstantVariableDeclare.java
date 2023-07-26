d03bb1b0e41d90d053d6da872a6c35e7e8e69e79
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-07-26 16:47:39 +0900

    Add files via upload

--- 20230726/variableex/ConstantVariableDeclare.java
+++ 20230726/variableex/ConstantVariableDeclare.java
@@ -4,7 +4,10 @@ public class ConstantVariableDeclare {
 
 	public static void main(String[] args) {
 		final double PI = 3.14;
+		String ADMIN_NAME = "root";
+		
 //		PI = 3.3333; // Error
+//		PI = 3.14159; // Error
 	}
 
 }
