c7d99a5d95da3698d782c7373aaf45854fec33cd
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-07-31 09:49:05 +0900

    Add files via upload

--- 20230728/iterationex/ForTest.java
+++ 20230728/iterationex/ForTest.java
@@ -42,7 +42,7 @@ public class ForTest {
 		
 //		1부터 20까지의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합을 구하시오.
 		int sum = 0;
-		for (int i=1; i<=100; i++) {
+		for (int i=1; i<=20; i++) {
 			if (!(i % 2 == 0 | i % 3 == 0)) {
 				sum += i;
 			}
