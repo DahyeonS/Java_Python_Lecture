509fea3bea1d8f2eec34c0dfdea3d2b24b837fe2
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-07-31 17:01:58 +0900

    Add files via upload

--- 20230731/iterationex/CumulateEx2.java
+++ 20230731/iterationex/CumulateEx2.java
@@ -3,7 +3,13 @@ package iterationex;
 public class CumulateEx2 {
 
 	public static void main(String[] args) {
-		
+		int i = 1;
+		int sum = 0;
+		while (i < 6) {
+			sum += i;
+			i++;
+		}
+		System.out.println(sum);
 	}
 
 }
--- 20230731/iterationex/WhileEx2.java
+++ 20230731/iterationex/WhileEx2.java
@@ -3,7 +3,16 @@ package iterationex;
 public class WhileEx2 {
 
 	public static void main(String[] args) {
-		
+		int i = 0;
+		while (i < 5) {
+			int j = 1;
+			while (j < 6) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
 	}
 
 }
