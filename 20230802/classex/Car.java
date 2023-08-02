2dd9ff2abc0e49f222c708ae1079b3722148967b
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-02 17:37:30 +0900

    Add files via upload

---
+++ 20230802/classex/Car.java
@@ -0,0 +1,15 @@
+package classex;
+
+public class Car {
+	static String color;
+	String company;
+	int door;
+	
+	void go() {
+		System.out.printf("%s %s가 전진합니다.\n", color, company);
+	}
+	
+	void back() {
+		System.out.printf("%s %s가 후진합니다.\n", color, company);
+	}
+}
