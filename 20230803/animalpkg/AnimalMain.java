b0f1a6e19fa840c9ca30034b4e946d3e743e384f
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-03 11:21:20 +0900

    Add files via upload

---
+++ 20230803/animalpkg/AnimalMain.java
@@ -0,0 +1,11 @@
+package animalpkg;
+
+import animalpkg.dogpkg.PublicDog;
+
+public class AnimalMain {
+
+	public static void main(String[] args) {
+		PublicDog publicDog = new PublicDog();
+	}
+
+}
