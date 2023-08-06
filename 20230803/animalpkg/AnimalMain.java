380dcb3b2d018b510b43270e15e98f8852e9c660
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-06 17:28:02 +0900

    Add files via upload

--- 20230803/animalpkg/AnimalMain.java
+++ 20230803/animalpkg/AnimalMain.java
@@ -1,11 +1,15 @@
 package animalpkg;
 
+import animalpkg.catpkg.ProtectedDogUseCat;
 import animalpkg.dogpkg.PublicDog;
 
 public class AnimalMain {
 
 	public static void main(String[] args) {
 		PublicDog publicDog = new PublicDog();
+//		DefaultDog = defaultDog = new DefaultDog(); // Error!!
+		
+		ProtectedDogUseCat protectedDogUseCat = new ProtectedDogUseCat();
 	}
 
 }
