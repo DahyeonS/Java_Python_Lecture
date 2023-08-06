954c0a36673e92f4cf6a6b76dedfbc7e113f5dbc
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-06 17:29:55 +0900

    Add files via upload

---
+++ 20230804/polymorphismpkg/AnimalPolyMain.java
@@ -0,0 +1,27 @@
+package polymorphismpkg;
+
+import inheritancepkg.Animal;
+import inheritancepkg.Cat;
+import inheritancepkg.Dog;
+import inheritancepkg.Eagle;
+import inheritancepkg.FinalAnimal;
+
+public class AnimalPolyMain {
+
+	public static void main(String[] args) {
+		Animal animal1 = new Dog("dog1", 1);
+		Animal animal2 = new Cat("cat1", 2);
+		Animal animal3 = new Eagle("eagle3", 3);
+//		Animal[] animalArr = new Animal[3];
+//		animalArr[0] = animal1;
+//		animalArr[2] = animal2;
+//		animalArr[3] = animal3;
+		
+		Dog dog = (Dog) animal1;
+		Cat cat = (Cat) animal2;
+		Eagle eagle = (Eagle) animal3;
+		
+		FinalAnimal finalAnimal = new FinalAnimal("fianl1", 4);
+	}
+
+}
