cd18ff67dc4a537d60e90a325a0af5812381e2b5
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-08 12:36:08 +0900

    Add files via upload

---
+++ 20230808/javaapipkg/Fruit.java
@@ -0,0 +1,17 @@
+package javaapipkg;
+
+public class Fruit {
+	String name;
+	String color;
+	
+	public Fruit(String name, String color) {
+		super();
+		this.name = name;
+		this.color = color;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+}
