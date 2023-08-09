9f96ef8b1e6a96b1b77eef3d1c113b2ea088202b
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-09 09:47:44 +0900

    Add files via upload

---
+++ 20230809/javaapipkg/CalendarEx.java
@@ -0,0 +1,13 @@
+package javaapipkg;
+
+import java.util.Calendar;
+
+public class CalendarEx {
+
+	public static void main(String[] args) {
+		Calendar today = Calendar.getInstance();
+		int month = today.MONTH;
+		System.out.println(month);
+	}
+
+}
