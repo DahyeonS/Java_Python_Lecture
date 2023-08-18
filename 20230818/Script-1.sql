e1c15c9248f2a807d00ea5c3ffb81fbc768aac16
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-18 15:43:26 +0900

    Add files via upload

--- 20230818/Script-1.sql
+++ 20230818/Script-1.sql
@@ -1,3 +1,12 @@
 SELECT SUBSTR('1994-06-09', 1, 4);
 SELECT SUBSTR('1994-06-09', 6, 2);
 SELECT SUBSTR('1994-06-09', 9);
+
+SELECT STRFTIME('%Y-%m-%d %H %M %S', 'now', 'localtime') as 'yyyy-mm-dd'; -- 'now' 현재 시간, 'localtime' 현지 시간 기준(GMT+9)
+
+-- 시스템 시간
+SELECT CURRENT_DATE;
+SELECT CURRENT_TIME;
+SELECT CURRENT_TIMESTAMP;
+
+SELECT SQlite_VERSION();
--- 20230818/Script.sql
+++ 20230818/Script.sql
@@ -1,3 +1,4 @@
+-- SELECT
 SELECT * FROM person WHERE id = 5;
 
 SELECT * FROM person WHERE name = '박혜리';
@@ -26,10 +27,12 @@ INSERT INTO pets VALUES (3, 'Ripley', 'Cat');
 INSERT INTO pets VALUES (4, 'Tom', 'Cat');
 SELECT * FROM pets;
 
+-- 칼럼 추가
 ALTER TABLE person ADD COLUMN new2 INTEGER NOT NULL DEFAULT 0; -- 칼럼 추가
 ALTER TABLE person DROP new2;
 ALTER TABLE person RENAME COLUMN new to height;
 
+-- 데이터 수정
 UPDATE person SET height = 167 WHERE id = 3;
 UPDATE person SET height = 170.3 WHERE id = 4;
 UPDATE person SET height = 164 WHERE id = 5;
@@ -37,19 +40,66 @@ SELECT * FROM person;
 
 -- Person 테이블에 Weight 칼럼을 추가해보라.
 ALTER TABLE person ADD COLUMN weight INTEGER NOT NULL DEFAULT 0;
-SELECT * FROM person;
-
--- 별명주기 AS
-SELECT name AS 이름, birthday 생일 FROM person;
 
 UPDATE person SET weight = 50 WHERE id = 1;
 UPDATE person SET weight = 0 WHERE id = 2;
 UPDATE person SET weight = 54 WHERE id = 4;
 UPDATE person SET weight = 46 WHERE id = 5;
+SELECT * FROM person;
 
+-- 별명주기 AS
+SELECT name AS 이름, birthday 생일 FROM person;
 
+-- ROUND()
 SELECT name, height, weight, ROUND(weight / (height * height * 0.0001), 1) bmi FROM person;
 
+-- VIEW
 CREATE VIEW brithdayview AS SELECT name, birthday bdate,
-SUBSTR(birthday, 1, 4), SUBSTR(birthday, 6, 2), SUBSTR(birthday, 9, 2) FROM person;
+SUBSTR(birthday, 1, 4) yyyy, SUBSTR(birthday, 6, 2) mm, SUBSTR(birthday, 9, 2) dd FROM person;
 SELECT * FROM brithdayview;
+SELECT * FROM brithdayview WHERE yyyy > '1992';
+
+-- CASE 조건문
+SELECT name, bdate, mm,
+CASE
+	WHEN mm = '01' THEN 'Jan.'
+	WHEN mm = '02' THEN 'Feb.'
+	WHEN mm = '03' THEN 'Mar.'
+	WHEN mm = '06' THEN 'Jun.'
+END as month
+FROM brithdayview;
+
+-- 날짜
+SELECT name, birthday 생일, STRFTIME('%Y', 'now') - SUBSTR(birthday, 1, 4) "만 나이", (STRFTIME('%m-%d', 'now') < SUBSTR(birthday, 6)) FROM person;
+
+-- 집계함수
+SELECT COUNT(*) FROM person;
+SELECT COUNT(id) FROM person; -- count() 함수는 보통 기본키를 넣음
+SELECT COUNT(height) FROM person; -- NULL값은 집계되지 않음
+
+SELECT MAX(height) FROM person;
+SELECT name, MAX(height) FROM person; -- 원래는 실행 불가 구문
+SELECT name, SUM(height) FROM person; -- 원래는 실행 불가 구문
+
+SELECT SUM(height) / COUNT(id) FROM person; -- 125.325
+SELECT AVG(height) FROM person; -- 167.1(NULL값 제외)
+
+UPDATE person SET height = 166.8 WHERE id = 1;
+SELECT * FROM person;
+SELECT AVG(height) FROM person; -- 167.025
+
+SELECT ROUND(height), COUNT(*) FROM person GROUP BY ROUND(height); -- 키 별 카운트
+SELECT ROUND(height), COUNT(*) FROM person GROUP BY ROUND(height) HAVING COUNT(*) > 1;
+
+--12.3 pets 테이블(연습문제 5.3)에서 고양이가 몇 마리인지 세어보라.
+SELECT COUNT(*) FROM pets WHERE animal = 'Cat';
+
+--pets 테이블에는 동물이 종류별로 몇 마리씩 있는지 세어보라.
+SELECT animal, COUNT(*) FROM pets GROUP BY animal;
+
+-- OVER(3.25 버전 이상에서만 사용 가능)
+SELECT name, height, AVG(height) OVER () AS mean FROM person;
+
+
+-- 표준편차
+SELECT name, height, AVG(height) OVER () AS mean, (height - AVG(height) OVER ()) 편차 FROM person;
