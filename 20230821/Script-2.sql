76ba15e01f45eb98530f243456b174d4f86b8206
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-21 15:18:46 +0900

    Add files via upload

--- 20230821/Script-2.sql
+++ 20230821/Script-2.sql
@@ -1,3 +1,5 @@
+--USE memberdb;
+
 --SELECT DISTINCT Country FROM Customers;
 --SELECT COUNT(DISTINCT country) FROM Customers;
 SELECT * FROM Customers WHERE Country <> 'Mexico';
@@ -6,3 +8,29 @@ SELECT * FROM Orders WHERE Freight > 50;
 
 SELECT COUNT(DISTINCT country) FROM Customers;
 SELECT * FROM Customers WHERE Country = 'Germany' AND City = 'Berlin';
+SELECT * FROM Customers WHERE Country = 'Germany' OR City = 'Berlin';
+
+SELECT * FROM Customers WHERE Country = 'Germany' AND (City = 'Berlin' OR City = 'Lulea');
+
+SELECT * FROM Customers WHERE Country = 'Germany' AND NOT Country = 'USA';
+SELECT * FROM Customers WHERE NOT Country = 'Germany' AND NOT Country = 'USA';
+SELECT * FROM Customers WHERE NOT (Country = 'Germany' AND NOT Country = 'USA');
+
+SELECT * FROM Customers ORDER BY Country;
+SELECT City, Country FROM Customers ORDER BY Country;
+SELECT City, Country FROM Customers ORDER BY Country, City;
+SELECT City, Country FROM Customers ORDER BY Country, City DESC;
+
+SELECT * FROM Customers WHERE Fax = NULL; -- X
+SELECT * FROM Customers WHERE Fax != NULL; -- X
+SELECT * FROM Customers WHERE Fax IS NULL;
+SELECT * FROM Customers WHERE Fax IS NOT NULL;
+SELECT * FROM Customers WHERE Fax IS NULL AND Region IS NULL;
+
+SELECT TOP 3 * FROM Products;
+
+SELECT MIN(UnitPrice) FROM Products;
+SELECT MAX(UnitPrice) FROM Products;
+SELECT COUNT(ProductID) FROM Products;
+SELECT AVG(UnitPrice) FROM Products;
+SELECT SUM(UnitPrice) / COUNT(UnitPrice) FROM Products;
