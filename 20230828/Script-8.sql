ad00079773c3e79ccba9a4b462070597d04bb255
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-28 11:45:21 +0900

    Add files via upload

--- 20230828/Script-8.sql
+++ 20230828/Script-8.sql
@@ -1,10 +1,1 @@
-SELECT * FROM dept;
-
-DESC dept;
-
-SELECT DISTINCT deptno FROM emp;
-
-SELECT ename, sal, sal * 12 + comm total FROM emp;
-
-SELECT * FROM emp ORDER BY sal ASC;
-SELECT * FROM emp ORDER BY sal DESC;
+SELECT * FROM DEPT;
