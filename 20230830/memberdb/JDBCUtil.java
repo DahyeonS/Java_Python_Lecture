bf65c4c62301dcab4a9075a31337168fe9c94f00
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-30 15:38:37 +0900

    Add files via upload

---
+++ 20230830/memberdb/JDBCUtil.java
@@ -0,0 +1,13 @@
+package memberdb;
+
+import java.sql.Connection;
+
+public class JDBCUtil {
+
+	public static Connection getConnection() {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		Connection conn = null;
+		
+		return conn;
+	}
+}
