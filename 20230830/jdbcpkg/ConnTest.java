35eca6c9048af824014b29e7e16415f554cefc20
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-08-30 12:58:03 +0900

    Add files via upload

--- 20230830/jdbcpkg/ConnTest.java
+++ 20230830/jdbcpkg/ConnTest.java
@@ -1,18 +1,35 @@
 package jdbcpkg;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnTest {
 
 	public static void main(String[] args) {
-//		1. 연결에 사용할 참조변수 생성
+//		1. 참조변수 설정
 		Connection conn = null;
-//		2. 드라이버 로딩
-		String driver = "com.mysql.cj.jdbc.Driver";
 		try {
+//		2. 드라이버 로딩
+			String driver = "com.mysql.cj.jdbc.Driver";
 			Class.forName(driver);
+//		3. DB 연결
+//			String url = "jdbc:mysql://localhost:3306/kdigital2307";
+//			url += "?ServerTimezone=UTC";
+			String url = "jdbc:mysql://localhost:3306/kdigital2307?ServerTimezone=UTC";
+			String user = "root";
+			String password = "rpass";
+			conn = DriverManager.getConnection(url, user, password);
+			
+			System.out.println("Connection OK!!");
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
