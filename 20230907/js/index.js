2616a68add0ff02ddba453a55f07be720967abd7
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-09-08 09:37:17 +0900

    Add files via upload

---
+++ 20230907/js/index.js
@@ -0,0 +1,30 @@
+// 1장
+// 01
+/*
+console.log('Hello JavaScript!');
+console.log(1 + 2 + 3 + 4);
+
+console.log('안녕하세요!');
+console.log('JavaScript를 배워봅시다');
+*/
+
+// 02
+/*
+let value = 1;
+console.log(value);
+value = 2;
+console.log(value);
+// let value = 2;
+
+const a = 1;
+// a = 2;
+*/
+let value = 1;
+let text = 'hello';
+let name = '자봐스크립트';
+let good = true;
+let loading = false;
+
+const friend = null;
+let criminal; // undefined
+console.log(criminal);
