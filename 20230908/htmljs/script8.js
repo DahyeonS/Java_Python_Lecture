48c6329c69c16b81b1be17c3c1f024cd90062af2
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-09-08 11:11:30 +0900

    Delete 20230908/htmljs/script8.js

--- 20230908/htmljs/script8.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    const clock = document.querySelector('#clock');
-    setInterval(function() {
-        const now = new Date();
-        clock.innerHTML = now.toLocaleString();
-    }, 1000); // milliseconds
-
-    console.log('bbbb');
-};
