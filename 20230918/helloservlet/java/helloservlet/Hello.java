f1a2bc6412a51b8dfa6a52fedb9e8805779cc29c
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-09-18 11:53:09 +0900

    Add files via upload

--- 20230918/helloservlet/java/helloservlet/Hello.java
+++ 20230918/helloservlet/java/helloservlet/Hello.java
@@ -9,13 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Hello")
+@WebServlet("/hi")
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public Hello() {
-        super();
-    }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
