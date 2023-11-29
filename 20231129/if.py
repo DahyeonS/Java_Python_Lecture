df5946d9101ee165901fe299e7a25070c6a1d7fa
Dahyeon Seo <siren199704@gmail.com>
2023-11-29 15:53:07 +0900

    Update if.py

--- 20231129/if.py
+++ 20231129/if.py
@@ -1,8 +1,8 @@
-# n = int(input('n >> '))
-# if n % 2 == 0 :
-#     print('짝수')
-# else :
-#     print('홀수')
+n = int(input('n >> '))
+if n % 2 == 0 :
+    print('짝수')
+else :
+    print('홀수')
 
 import datetime
 
@@ -20,3 +20,42 @@ else :
 
 date = ['월', '화', '수', '목', '금', '토', '일']
 print(date[day] + '요일')
+
+if day == 0 :
+    print('월')
+elif day == 1 :
+    print('화')
+elif day == 2 :
+    print('수')
+elif day == 3 :
+    print('목')
+elif day == 4 :
+    print('금')
+elif day == 5 :
+    print('토')
+else :
+    print('일')
+
+if day % 2 == 0 :
+    if day == 0 :
+        print('월')
+    else :
+        print('월요일은 아니에요')
+else :
+    pass # 아무 작업도 수행하지 않음
+
+num = 80
+if num >= 60 :
+    grade = 'pass'
+else :
+    grade = 'not pass'
+print(grade)
+
+grade = 'pass' if num >= 60 else 'not pass' # 삼항 연산자(참 if 조건 else 거짓)
+print(grade)
+
+s = 'hong is hong gil dong'
+if 'hong' in s :
+    print('ok')
+else :
+    print('not found')
