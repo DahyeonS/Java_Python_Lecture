a493720e371e1f212207c6e97d659bfd4c7830aa
Dahyeon Seo <siren199704@gmail.com>
2023-12-04 15:39:28 +0900

    Update test.py

--- 20231204/jupyter/test.py
+++ 20231204/jupyter/test.py
@@ -3,4 +3,12 @@ import  my_module
 print(my_module.my_var)
 
 my_class = my_module.My_class()
-my_class.my_method(), my_class.my_def()
+my_class.my_method()
+my_module.my_def()
+
+from my_module import My_class, my_var, my_def
+
+print(my_var)
+my_class = My_class()
+my_class.my_method()
+my_def()
