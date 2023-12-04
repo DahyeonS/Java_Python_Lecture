e1c8d78ba19d007696bd3162b5ba841fd055dd5d
Dahyeon Seo <siren199704@gmail.com>
2023-12-04 15:49:58 +0900

    Update test2.py

--- 20231204/jupyter/test2.py
+++ 20231204/jupyter/test2.py
@@ -1,7 +1,6 @@
 import my_package.my_module2
 
 print(my_package.my_module2.my_var)
-
 my_class = my_package.my_module2.My_class()
 my_class.my_method()
 my_package.my_module2.my_def()
@@ -9,7 +8,6 @@ my_package.my_module2.my_def()
 from my_package import my_module2
 
 print(my_module2.my_var)
-
 my_class = my_module2.My_class()
 my_class.my_method()
 my_module2.my_def()
@@ -17,7 +15,6 @@ my_module2.my_def()
 from my_package.my_module2 import my_var, My_class, my_def
 
 print(my_var)
-
 my_class = My_class()
 my_class.my_method()
 my_def()
@@ -25,7 +22,6 @@ my_def()
 import my_package.my_module2 as mm
 
 print(mm.my_var)
-
 my_class = mm.My_class()
 my_class.my_method()
 mm.my_def()
