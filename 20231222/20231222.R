2cc2ccc2e5713d17c0387cab911bea0adb6799ad
Dahyeon Seo <siren199704@gmail.com>
2023-12-27 18:12:48 +0900

    Update 20231222.R

--- 20231222/20231222.R
+++ 20231222/20231222.R
@@ -1,5 +1,6 @@
 # 데이터 가공
 library(dplyr)
+library(ggplot2)
 
 exam <- read.csv('./Lecture/RMD/csv_exam.csv')
 exam
@@ -56,7 +57,9 @@ mean(class2$math) # 61.25
 
 # %/% - 몫, %% - 나머지
 
-# mpg 데이터
+# mpg 데이터 다루기
+mpg
+
 mpg_df <- as.data.frame(mpg)
 mpg_df
 
@@ -180,3 +183,79 @@ mpg_df %>%
   mutate(total = cty + hwy, mean = total / 2) %>%
   arrange(desc(mean)) %>%
   head(3)
+
+
+test1 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    midterm = c(60, 70, 80, 90, 85))
+
+test2 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    final = c(70, 83, 65, 95, 80))
+
+test1
+test2
+
+total <- left_join(test1, test2, by = "id") # 상따옴표 입력
+total
+
+name <- data.frame(class = c(1, 2, 3, 4, 5),
+                   teacher = c('kim', 'lee', 'park', 'choi', 'jung'))
+name
+
+exam_new <- left_join(exam, name, by ="class")
+exam_new
+
+group_a <- data.frame(id = c(1, 2, 3, 4, 5),
+                      test = c(60, 70, 80, 90, 85))
+group_b <- data.frame(id = c(6, 7, 8, 9, 10),
+                      test = c(70, 83, 65, 95, 80))
+group_a
+group_b
+
+group_all <- bind_rows(group_a, group_b)
+group_all
+
+# mpg 데이터 다루기
+mpg_df <- as.data.frame(mpg)
+fuel <- data.frame(fl = c('c', 'd', 'e', 'p', 'r'),
+                   price_fl = c(2.35, 2.38, 2.11, 2.76, 2.22),
+                   stringsAsFactors = F)
+fuel
+
+# 1번 문제
+mpg_df <- left_join(mpg_df, fuel, by = "fl")
+mpg_df
+
+# 2번 문제
+mpg_df %>%
+  select('model', 'fl', 'price_fl') %>%
+  head(5)
+
+
+# 분석 도전
+midwest <- as.data.frame(midwest)
+
+# 1번
+midwest <- midwest %>%
+  mutate(ratio_child = (df$poptotal - df$popadults) / df$poptotal)
+midwest$ratio_child
+
+# 2번
+midwest %>%
+  arrange(desc(ratio_child)) %>%
+  select(county, ratio_child) %>%
+  head(5)
+
+# 3번
+midwest <- midwest %>%
+  mutate(grade_child = ifelse(ratio_child >= 0.4, 'large',
+                              ifelse(ratio_child >= 0.3, 'middle', 'small')))
+table(midwest$grade_child)
+
+# 4번
+midwest <- midwest %>%
+  mutate(ratio_asian = (df$poptotal - df$popasian) / df$poptotal)
+
+midwest %>%
+  arrange(desc(ratio_asian)) %>%
+  select(state, county, ratio_asian) %>%
+  tail(10)
