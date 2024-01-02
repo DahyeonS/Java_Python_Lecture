f27294f6177f18480a0b0d9df99efe4814a90666
Dahyeon Seo <siren199704@gmail.com>
2024-01-02 17:46:35 +0900

    Update 20231222.R

--- 20231222/20231222.R
+++ 20231222/20231222.R
@@ -358,3 +358,16 @@ mpg[c(10, 14, 58, 93), 'drv'] <- 'k'
 mpg[c(29, 43, 129, 203), 'cty'] <- c(3, 4, 39, 42)
 
 # 1번 문제
+table(mpg$drv)
+mpg$drv <- ifelse(mpg$drv %in% 'k', NA, mpg$drv)
+
+# 2번 문제
+boxplot(mpg$cty)
+boxplot(mpg$cty)$stats
+mpg$cty <- ifelse(mpg$cty < 9 | mpg$cty > 26, NA, mpg$cty)
+
+# 3번 문제
+mpg %>%
+  filter(!is.na(drv)) %>%
+  group_by(drv) %>%
+  summarise(mean_cty = mean(cty, na.rm = T))
