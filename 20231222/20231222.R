a351ba4b2b00ddb01b70cd6f56ed0e51bd4c0c82
Dahyeon Seo <siren199704@gmail.com>
2024-01-02 17:20:29 +0900

    Update 20231222.R

--- 20231222/20231222.R
+++ 20231222/20231222.R
@@ -236,7 +236,7 @@ midwest <- as.data.frame(midwest)
 
 # 1번
 midwest <- midwest %>%
-  mutate(ratio_child = (df$poptotal - df$popadults) / df$poptotal)
+  mutate(ratio_child = (midwest$poptotal - midwest$popadults) / midwest$poptotal)
 midwest$ratio_child
 
 # 2번
@@ -275,3 +275,86 @@ table(is.na(df$score))
 
 mean(df$score) # NA
 sum(df$score) # NA
+
+df %>% filter(is.na(score)) # 결측치 출력
+df %>% filter(!is.na(score))
+
+df_nomiss <- df %>% filter(!is.na(score))
+mean(df_nomiss$score) # 4
+sum(df_nomiss$score) # 16
+
+df_nomiss <- df %>% filter(!is.na(score) & !is.na(sex))
+df_nomiss
+
+df_nomiss2 <- na.omit(df)
+df_nomiss2
+
+mean(df$score, na.rm = T) # 4
+sum(df$score, na.rm = T) # 16
+
+exam <- read.csv('./Lecture/RMD/csv_exam.csv')
+exam[c(3, 8, 15), 'math'] <- NA
+
+exam %>% summarise(mean_math = mean(math)) # NA
+exam %>% summarise(mean_math = mean(math, na.rm = T)) # 55.23529
+
+exam %>% summarise(mean_math = mean(math, na.rm = T),
+                   sum_math = sum(math, na.rm = T),
+                   median_math = median(math, na.rm = T))
+
+mean(exam$math, na.rm = T) # 55.23529
+exam$math <- ifelse(is.na(exam$math), 55, exam$math)
+table(is.na(exam$math))
+exam
+mean(exam$math) # 55.2
+
+# mpg 결측치 처리
+mpg <- as.data.frame(ggplot2::mpg)
+mpg[c(65, 124, 131, 153, 212), 'hwy'] <- NA
+
+# 1번 문제
+table(is.na(mpg$drv)) # 0
+table(is.na(mpg$hwy)) # 5
+
+# 2번 문제
+mpg %>%
+  filter(!is.na(hwy)) %>%
+  group_by(drv) %>%
+  summarise(hwy_mean = mean(hwy))
+
+
+outlier <- data.frame(sex = c(1, 2, 1, 3, 2, 1),
+                      score = c(5, 4, 3, 4, 2, 6))
+outlier
+
+table(outlier$sex)
+table(outlier$score)
+
+outlier$sex <- ifelse(outlier$sex == 3, NA, outlier$sex)
+outlier
+
+outlier$score <- ifelse(outlier$score > 5, NA, outlier$score)
+outlier
+
+outlier %>%
+  filter(!is.na(sex) & !is.na(score)) %>%
+  group_by(sex) %>%
+  summarise(mean_score = mean(score))
+
+mpg <- as.data.frame(ggplot2::mpg)
+boxplot(mpg$hwy)
+boxplot(mpg$hwy)$stats # 12 ~ 37
+
+mpg$hwy <- ifelse(mpg$hwy < 12 | mpg$hwy > 37, NA, mpg$hwy)
+table(is.na(mpg$hwy))
+
+mpg %>%
+  group_by(drv) %>%
+  summarise(mean_hwy = mean(hwy, na.rm = T))
+
+# mpg 이상치 처리
+mpg <- as.data.frame(ggplot2::mpg)
+mpg[c(10, 14, 58, 93), 'drv'] <- 'k'
+mpg[c(29, 43, 129, 203), 'cty'] <- c(3, 4, 39, 42)
+
+# 1번 문제
